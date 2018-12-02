ab6136a9afebf17a7baa6063c8372a84cf43829a
pipsrocha <pipa_sr@hotmail.com>
2018-12-02 19:23:34 +0000

    minor changes

--- js/usamap.js
+++ js/usamap.js
@@ -547,75 +547,103 @@ function gen_chord() {
 }
 
 function gen_summ() {
-  // set the dimensions and margins of the graph
-  var width = 500,
-      height = 500;
+// Set the dimensions of the canvas / graph
+var margin = {top: 10, right: 20, bottom: 70, left: 40},
+    width = 600 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
 
-  // parse the date / time
-  var parseTime = d3v4.timeParse("%d-%b-%y");
+// Parse the date / time
+var parseDate = d3.time.format("%b %Y").parse;
 
-  // set the ranges
-  var x = d3v4.scaleTime().range([0, width]);
-  var y = d3v4.scaleLinear().range([height, 0]);
+// Set the ranges
+var x = d3.time.scale().range([0, width]);
+var y = d3.scale.linear().range([height, 0]);
 
-  // define the 1st line
-  var valueline = d3v4.line()
-      .x(function(d) { return x(d.date); })
-      .y(function(d) { return y(d.close); });
+// Define the axes
+var xAxis = d3.svg.axis().scale(x)
+    .orient("bottom").ticks(5);
 
-  // define the 2nd line
-  var valueline2 = d3v4.line()
-      .x(function(d) { return x(d.date); })
-      .y(function(d) { return y(d.open); });
+var yAxis = d3.svg.axis().scale(y)
+    .orient("left").ticks(5);
 
-  // append the svg obgect to the body of the page
-  // appends a 'group' element to 'svg'
-  // moves the 'group' element to the top left margin
-  var svg = d3.select("#summ")
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .append("g")
-      .attr("transform","translate(0,0)");
+// Define the line
+var priceline = d3.svg.line() 
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.price); });
+    
+// Adds the svg canvas
+var svg = d3.select("#summ")
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform", 
+              "translate(" + margin.left + "," + margin.top + ")");
 
-  // Get the data
-  d3.csv("data/data2.csv", function(error, data) {
-    if (error) throw error;
-
-    // format the data
+// Get the data
+d3.csv("data/stocks.csv", function(error, data) {
     data.forEach(function(d) {
-        d.date = parseTime(d.date);
-        d.close = +d.close;
-        d.open = +d.open;
+    d.date = parseDate(d.date);
+    d.price = +d.price;
     });
 
     // Scale the range of the data
     x.domain(d3.extent(data, function(d) { return d.date; }));
-    y.domain([0, d3.max(data, function(d) {
-      return Math.max(d.close, d.open); })]);
+    y.domain([0, d3.max(data, function(d) { return d.price; })]);
 
-    // Add the valueline path.
-    svg.append("path")
-        .data([data])
-        .attr("class", "line")
-        .attr("d", valueline);
+    // Nest the entries by symbol
+    var dataNest = d3.nest()
+        .key(function(d) {return d.symbol;})
+        .entries(data);
 
-    // Add the valueline2 path.
-    svg.append("path")
-        .data([data])
-        .attr("class", "line")
-        .style("stroke", "red")
-        .attr("d", valueline2);
+    var color = d3.scale.category10();   // set the colour scale
+
+    legendSpace = width/dataNest.length; // spacing for the legend
+
+    // Loop through each symbol / key
+    dataNest.forEach(function(d,i) { 
+
+        svg.append("path")
+            .attr("class", "line")
+            .style("stroke", function() { // Add the colours dynamically
+                return d.color = color(d.key); })
+            .attr("id", 'tag'+d.key.replace(/\s+/g, '')) // assign ID
+            .attr("d", priceline(d.values))
+            .attr("fill", "none");
+
+        // Add the Legend
+        svg.append("text")
+            .attr("x", (legendSpace/2)+i*legendSpace)  // space legend
+            .attr("y", height + (margin.bottom/2)+ 5)
+            .attr("class", "legend")    // style the legend
+            .style("fill", function() { // Add the colours dynamically
+                return d.color = color(d.key); })
+            .on("click", function(){
+                // Determine if current line is visible 
+                var active   = d.active ? false : true,
+                newOpacity = active ? 0 : 1; 
+                // Hide or show the elements based on the ID
+                d3.select("#tag"+d.key.replace(/\s+/g, ''))
+                    .transition().duration(100) 
+                    .style("opacity", newOpacity); 
+                // Update whether or not the elements are active
+                d.active = active;
+                })  
+            .text(d.key); 
+
+    });
 
     // Add the X Axis
     svg.append("g")
+        .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3v4.axisBottom(x));
+        .call(xAxis);
 
     // Add the Y Axis
     svg.append("g")
-        .call(d3v4.axisLeft(y));
+        .attr("class", "y axis")
+        .call(yAxis);
 
-  });
+});
 
 }
